5cb59335a729b3872d38e39e3432d59d4670d61d
andrey-kolesnikov-1 <andrey.kolesnikov1605@gmail.com>
2020-07-08 21:46:57 +0300

    v 1.02

--- calendar/script.js
+++ calendar/script.js
@@ -11,12 +11,11 @@ let currentDate = {
     year: new Date().getFullYear(),
     month: new Date().getMonth() + 1
 };
-let arrNotes = [];
 let thisDay;
 let index;
 let objectDay;
 let mapNotes = new Map();
-console.log('это новая ветка')
+let selectedDay;
 
 class Notes { // класс для работы с заметками
     constructor(year, month, day, note = '') {
@@ -45,23 +44,34 @@ header.addEventListener('click', (event) => {
     });
 });
 
+// функция переключения на один месяц назад
+function prevMonth() {
+    currentDate.month -= 1;
+    createDays(date(currentDate));
+}
+
+// функция переключения на один месяц вперёд
+function nextMonth() {
+    currentDate.month += 1;
+    createDays(date(currentDate));
+}
+
 // обработка нажатия на конкретный день месяца
 days.addEventListener('click', (event) => {
     disableToolTip();
     let numDay = days.querySelectorAll('.day');
     numDay.forEach((value, key) => {
         if (event.target.closest('.day') == value) {
+            modalWindow.classList.add('modal_view');
             text.value = '';
             text.placeholder = '';
+            text.focus();
             index = key + 1;
             objectDay = value;
-            arrNotes.forEach((val) => {
-                if (val.fullDay === Notes.mathFullDay(currentDate.year, currentDate.month, index)) {
-                    text.value = val.note;
-                }
-            });
-            modalWindow.classList.add('modal_view');
-            text.focus();
+            selectedDay = Notes.mathFullDay(currentDate.year, currentDate.month, index);
+            if (mapNotes.has(selectedDay)) {
+                text.value = mapNotes.get(selectedDay).note;
+            }
         }
     });
 });
@@ -71,30 +81,29 @@ days.addEventListener('mouseover', (event) => {
     let numDay = days.querySelectorAll('.day');
     numDay.forEach((value, key) => {
         if (event.target.closest('.day') == value) {
-            arrNotes.forEach((val) => {
-                if (val.fullDay === Notes.mathFullDay(currentDate.year, currentDate.month, key + 1)) {
-                    let tooltipElem = document.querySelector('.tooltip');
-                    tooltipElem.style.display = 'block';
-                    let noteText = document.querySelector('.tooltiparea');
+            let index = Notes.mathFullDay(currentDate.year, currentDate.month, key + 1);
+            if (mapNotes.has(index)) {
+                let tooltipElem = document.querySelector('.tooltip');
+                tooltipElem.style.display = 'block';
+                let noteText = document.querySelector('.tooltiparea');
 
-                    let text = '';
-                    val.note.split('\n').forEach((str) => {
-                        text = text + str + '<br>';
-                    });
-                    noteText.innerHTML = text;
+                let text = '';
+                mapNotes.get(index).note.split('\n').forEach((str) => {
+                    text = text + str + '<br>';
+                });
+                noteText.innerHTML = text;
 
-                    // спозиционируем его сверху от аннотируемого элемента (top-center)
-                    let coords = value.getBoundingClientRect();
-                    let left = coords.left + (value.offsetWidth - tooltipElem.offsetWidth) / 2;
-                    if (left < 0) left = 0; // не заезжать за левый край окна
-                    let top = coords.top - tooltipElem.offsetHeight - 5;
-                    if (top < 0) { // если подсказка не помещается сверху, то отображать её снизу
-                        top = coords.top + value.offsetHeight + 5;
-                    }
-                    tooltipElem.style.left = left + 'px';
-                    tooltipElem.style.top = top + 'px';
+                // спозиционируем его сверху от аннотируемого элемента (top-center)
+                let coords = value.getBoundingClientRect();
+                let left = coords.left + (value.offsetWidth - tooltipElem.offsetWidth) / 2;
+                if (left < 0) left = 0; // не заезжать за левый край окна
+                let top = coords.top - tooltipElem.offsetHeight - 5;
+                if (top < 0) { // если подсказка не помещается сверху, то отображать её снизу
+                    top = coords.top + value.offsetHeight + 5;
                 }
-            });
+                tooltipElem.style.left = left + 'px';
+                tooltipElem.style.top = top + 'px';
+            }
         }
     });
 });
@@ -114,103 +123,80 @@ function disableToolTip() {
     toolTip.style.display = 'none';
 }
 
-// обработка нажатия на кнопки модального окна (появляется при клике на дни месяца)
-divBtnModal.addEventListener('click', (event) => {
-    btnModal.forEach((value, key) => {
-        if (event.target == value) {
-            switch (key) {
-                case 0: // нажата кнопка "Удалить"
-                    arrNotes.forEach((value, i) => {
-                        if (value.fullDay === Notes.mathFullDay(currentDate.year, currentDate.month, index)) {
-                            arrNotes.splice(i, 1);
-                            objectDay.classList.remove('select_day');
-                        }
-                    });
-                    closeModal();
-                    break;
-                case 1: // нажата кнопка "Отмена"
-                    closeModal();
-                    break;
-                case 2: // нажата кнопка "Принять"
-                    if (text.value.trim() === '') {
-                        text.value = '';
-                        text.placeholder = 'Заметка не должна быть пустой.';
-                        text.focus();
-                    } else {
-                        let note = new Notes(currentDate.year, currentDate.month, index, text.value);
-                        arrNotes.forEach((value, i) => {
-                            if (value.fullDay === note.fullDay) {
-                                arrNotes.splice(i, 1);
-                            }
-                        });
-                        arrNotes.push(note);
-                        objectDay.classList.add('select_day');
-                        closeModal();
-                    }
-                    break;
-            }
-        }
-    });
+// нажата кнопка "Удалить"
+document.getElementById('btnDelete').addEventListener('click', () => {
+    mapNotes.delete(selectedDay);
+    objectDay.classList.remove('select_day');
+    objectDay.classList.remove('select_day_currentDay');
+    closeModal();
+});
+
+// нажата кнопка "Отмена"
+document.getElementById('btnCansel').addEventListener('click', () => {
+    closeModal();
+});
+
+// нажата кнопка "Принять"
+document.getElementById('btnApply').addEventListener('click', () => {
+    if (text.value.trim() === '') {
+        text.value = '';
+        text.placeholder = 'Заметка не должна быть пустой.';
+        text.focus();
+    } else {
+        let note = new Notes(currentDate.year, currentDate.month, index, text.value);
+        mapNotes.set(selectedDay, note);
+        objectDay.classList.add('select_day');
+        createDays(date(currentDate));
+        closeModal();
+    }
 });
 
 // функция закрытия модального окна
 function closeModal() {
-    modalWindow.classList.remove('modal_view');
-    modalWindow.classList.add('modal_not_view');
+    modalWindow.classList.replace('modal_view', 'modal_not_view');
     setTimeout(() => {
         modalWindow.classList.remove('modal_not_view');
     }, 400);
 }
 
-// функция переключения на один месяц назад
-function prevMonth() {
-    currentDate.month -= 1;
-    createDays(date(currentDate));
-}
-
-// функция переключения на один месяц вперёд
-function nextMonth() {
-    currentDate.month += 1;
-    createDays(date(currentDate));
-}
-
 // функция определения количества дней в месяце заданной даты и определение текущего месяца
-function date(obj, day = 0) {
-    let d = new Date(obj.year, obj.month, day);
-    let a = new Date();
-    (a.getFullYear() === obj.year && a.getMonth() + 1 === obj.month) ? thisDay = true: thisDay = false;
-    setInfoMonth(d);
-    return d.getDate();
+function date(obj) {
+    let selectDate = new Date(obj.year, obj.month, 0);
+    let currentDate = new Date();
+    thisDay = currentDate.getFullYear() === obj.year && currentDate.getMonth() + 1 === obj.month;
+    setInfoMonth(selectDate);
+    return selectDate.getDate();
 }
 
 // функция вывода названия месяца и года в заголовок календаря
-function setInfoMonth(d) {
+function setInfoMonth(date) {
     let month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-    infoMonth.textContent = `${month[d.getMonth()]} ${d.getFullYear()}`
+    infoMonth.textContent = `${month[date.getMonth()]} ${date.getFullYear()}`;
 }
 
 // функция генерации и вывода дней месяца
-function createDays(d) {
+function createDays(monthDays) {
     let day = document.querySelectorAll('.day');
     day.forEach((value) => {
         value.remove();
     })
 
-    let el, label, currDay = new Date().getDate();
-    for (let i = 1; i <= d; i++) {
+    let el, label, currentDay = new Date().getDate();
+    for (let i = 1; i <= monthDays; i++) {
         el = document.createElement("div");
         el.classList = 'day';
         label = document.createElement("label");
         label.textContent = '' + (i);
-        if (thisDay && i === currDay) {
+        if (thisDay && i === currentDay) {
             label.style.color = 'red';
             el.classList.add('currentDay');
-        }
-        arrNotes.forEach((value) => {
-            if (Notes.mathFullDay(currentDate.year, currentDate.month, i) === value.fullDay) {
-                el.classList.add('select_day');
+            if (mapNotes.has(Notes.mathFullDay(currentDate.year, currentDate.month, i))) {
+                el.classList.add('select_day_currentDay');
             }
-        });
+        }
+        if (mapNotes.has(Notes.mathFullDay(currentDate.year, currentDate.month, i))) {
+            el.classList.add('select_day');
+        }
         el.appendChild(label);
         days.appendChild(el);
     }
